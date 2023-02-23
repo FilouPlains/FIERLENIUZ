3d0068404bc3afb071274d98fcd30f0c61538171
Lucas <lucas.rouaud@gmail.com>
2023-02-23 15:39:42 +0100

    Adding a program to parse SCOPe classification items.

---
+++ src/scope_tree/scope_tree.py
@@ -0,0 +1,20 @@
+"""This module contains an object to create a SCOPe network graph.
+"""
+
+__authors__ = ["ROUAUD Lucas"]
+__contact__ = ["lucas.rouaud@gmail.com"]
+__date__ = "07/02/2023"
+__version__ = "1.0.0"
+__copyright__ = "CC BY-SA"
+
+
+
+class Scope:
+    """An object to create a SCOPe network graph.
+    """
+
+    def __init__(self) -> None:
+        pass
+
+    def __str__(self) -> str:
+        pass
