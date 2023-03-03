7ae5ab71711f48db640d935aabe226f5cbacbbc2
Lucas <lucas.rouaud@gmail.com>
2023-03-03 18:19:29 +0100

    Improving the '--help' message.

--- src/embeddings/arg_parser.py
+++ src/embeddings/arg_parser.py
@@ -5,7 +5,7 @@
 __authors__ = ["ROUAUD Lucas"]
 __contact__ = ["lucas.rouaud@gmail.com"]
 __date__ = "03/02/2023"
-__version__ = "1.0.0"
+__version__ = "1.2.3"
 __copyright__ = "CC BY-SA"
 
 
@@ -39,12 +39,17 @@ def parsing():
                         "Peitsch code. An command example, when you are at "
                         "the project's root (FIERLENIUS/) is:\n\n    "
                         "$ python3 peitsch2vec.py -i data/hace.out -o "
-                        "data/mintf_30/ -d data/HCDB_summary.csv")
+                        "data/mintf_30/ -d data/HCDB_summary.csv\n\nlegend:\n  "
+                        "int: Integer.\n  [type|value]: Type of the input "
+                        "required, follow by the default value. So if this "
+                        "optional arguments is not used, 'value' will be "
+                        "chosen.")
 
     # Setup the arguments parser object.
     parser: object = argparse.ArgumentParser(
         description=DESCRIPTION,
-        formatter_class=argparse.RawTextHelpFormatter
+        formatter_class=argparse.RawTextHelpFormatter,
+        add_help=False
     )
 
     # ==========
@@ -52,37 +57,61 @@ def parsing():
     # ARGUMENTS
     #
     # ==========
+
+    parser.add_argument(
+        "-h",
+        "--help",
+        action="help",
+        help="    > Display this help message, then exit the program."
+    )
+
+    parser.add_argument(
+        "-v",
+        "--version",
+        action="version",
+        version=f"Program version is {__version__}",
+        help="    > Display the program's version, then exit the program."
+    )
+
     # == REQUIRED.
     parser.add_argument(
-        "-i, --input",
+        "-i",
+        "--input",
         dest="input",
         required=True,
         type=str,
-        help="[[MANDATORY]]\n    ['.out'] A pyHCA segmentation results file."
+        metavar="[FILE]['.out']",
+        help="[[MANDATORY]]\n    > A pyHCA segmentation results file."
     )
 
     parser.add_argument(
-        "-o, --output",
+        "-o",
+        "--output",
         dest="output",
         required=True,
         type=str,
-        help="[[MANDATORY]]\n    A folder where the results will be stored."
+        metavar="[DIRECTORY]",
+        help="[[MANDATORY]]\n    > A folder where the results will be stored."
     )
 
     parser.add_argument(
-        "-d, --hcdb",
+        "-d",
+        "--hcdb",
         dest="hcdb",
         required=True,
         type=str,
-        help="[[MANDATORY]]\n    ['.csv'] The HC database path."
+        metavar="[FILE]['.csv']",
+        help="[[MANDATORY]]\n    > The HC database path."
     )
 
     parser.add_argument(
-        "-s, --scope",
+        "-s",
+        "--scope",
         dest="scope",
         required=True,
         type=str,
-        help="[[MANDATORY]]\n    ['.txt'] A SCOPe classification file."
+        metavar="[FILE]['.txt']",
+        help="[[MANDATORY]]\n    > A SCOPe classification file."
     )
 
     # == OPTIONAL.
@@ -90,52 +119,58 @@ def parsing():
         "--epochs",
         default=5,
         type=int,
-        help="[integer] Number of epochs, by default 5."
+        metavar="[int|5]",
+        help="    > Number of epochs."
     )
 
     parser.add_argument(
         "--mintf",
         default=30,
         type=int,
-        help=("[integer] Maximum term frequency, by default '30'. If 'None', "
-              "no threshold will be applied.")
+        metavar="[int|30]",
+        help=("    > Maximum term frequency. If 'None', no threshold will be "
+              "applied.")
     )
 
     parser.add_argument(
         "--maxtf",
         default=None,
         type=int,
-        help=("[integer] Maximum term frequency, by default 'None'. If 'None', "
-              "no threshold will be applied.")
+        metavar="[int|None]",
+        help=("    > Maximum term frequency. If 'None', no threshold will be "
+              "applied.")
     )
 
     parser.add_argument(
         "--minlen",
         default=2,
         type=int,
-        help="[integer] Minimum length of a hydrophobic cluster, by default 2."
+        metavar="[int|2]",
+        help="    > Minimum length of a hydrophobic cluster."
     )
 
     parser.add_argument(
         "--sample",
         default=1e-3,
         type=int,
-        help=("[float] Down sampling setting for frequent words, by default"
-              " 1e-3.")
+        metavar="[float|1e-3]",
+        help="    > Down sampling setting for frequent words."
     )
 
     parser.add_argument(
         "--size",
         default=300,
         type=int,
-        help="[integer] Size of the words embeddings vector, by default 300."
+        metavar="[int|300]",
+        help="    > Size of the words embeddings vector."
     )
 
     parser.add_argument(
         "--window",
         default=5,
         type=int,
-        help="[integer] Window size, by default 5."
+        metavar="[int|5]",
+        help="    > Window size."
     )
 
     # Check the computer's number of CPU.
@@ -146,7 +181,8 @@ def parsing():
         default=nb_cpu,
         required=False,
         type=int,
-        help=("[integer] Number of processes, by default set to your number "
+        metavar=f"[int|{nb_cpu}]",
+        help=("    > Number of processes, by default set to your number "
               f"of CPU. Here, {nb_cpu} CPU are detected.")
     )
 
@@ -154,8 +190,9 @@ def parsing():
         "--segment",
         required=False,
         action="store_true",
-        help=("If used, output sentence by foldable segment and not by "
-              "domain. By default this option inactivated.")
+        help=("    > If used, output sentence by foldable segment and not by "
+              "domain. By default this option is inactivated, so 'False' is "
+              "store.")
     )
 
     # Transform the input into a dictionary with arguments as key.
@@ -212,7 +249,7 @@ def parsing():
         sysexit(f"\n[Err##] Ask number of CPU, {argument['cpu']}, is "
                 "inferior or equal to 0. Please change the input number.")
 
-    to_check: "list[str]" = ["epochs","minlen", "size", "window"]
+    to_check: "list[str]" = ["epochs", "minlen", "size", "window"]
 
     if argument["mintf"] is not None:
         to_check += ["mintf"]
@@ -223,14 +260,14 @@ def parsing():
     for key in to_check:
         if argument[key] <= 0:
             sysexit(f"\n[Err##] The given arguments '{key}' is incorrect. "
-                    "You have to give an integer greater strictly than 0.")
+                    "You have to give an int greater strictly than 0.")
 
     if argument["mintf"] is not None and argument["maxtf"] is not None:
         if argument["mintf"] >= argument["maxtf"]:
             sysexit("\n[Err##] The given arguments for '--mintf' "
                     f"(= {argument['mintf']}) and for '--maxtf' (= "
                     f"{argument['maxtf']}) are incorrect. You have to give an "
-                    "integer so the maximum is strictly greater than the "
+                    "int so the maximum is strictly greater than the "
                     "minimum.")
 
     return argument
