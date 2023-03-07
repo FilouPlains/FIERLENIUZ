eae84b879f15b6df095ae2fc700f7099975681ec
Lucas <lucas.rouaud@gmail.com>
2023-03-07 11:19:45 +0100

    Changing greatly the object functioning. Adding a new metric.

--- src/embeddings/context_analyzer.py
+++ src/embeddings/context_analyzer.py
@@ -6,29 +6,30 @@ https://publications.mpi-cbg.de/Wu_1990_6334.pdf
 
 __authors__ = ["ROUAUD Lucas"]
 __contact__ = ["lucas.rouaud@gmail.com"]
-__date__ = "06/03/2023"
-__version__ = "1.0.0"
+__date__ = "07/03/2023"
+__version__ = "1.1.0"
 __copyright__ = "CC BY-SA"
 
 # [N]
 import numpy as np
 
 
-class Alignment:
-    """To create an object to align to set of objects. The algorithm is
-    available in `An O(NP) Sequence Comparison Algorithm` by Sun WU, Udi MANBER,
-    and Gene MYERS in Information Processing Letters, or in the next link:
-    https://publications.mpi-cbg.de/Wu_1990_6334.pdf
+class ContextAnalyzer:
+    """To create an object to align to set of objects. 
     """
 
-    def __init__(self, seq_a: str, seq_b: str) -> None:
-        """Instantiate an `Alignment` object.
+    def __init__(
+        self,
+        seq_a: "list | str | np.array",
+        seq_b: "list | str | np.array"
+    ) -> None:
+        """Instantiate an `ContextAnalyzer` object.
 
         Parameters
         ----------
-        seq_a : str
+        seq_a : list | str | np.array
             The first sequence-context to compare.
-        seq_b : str
+        seq_b : list | str | np.array
             The second sequence-context to compare.
         """
         # Gets sequence-context length.
@@ -46,25 +47,45 @@ class Alignment:
 
         # Create an array full of `-1`, of seq_a and seq_b length + 3.
         self.array: object = np.full((self.len_a + self.len_b + 3), -1)
-        self.score: int = None
+        self.distance: "list[int]" = [None, None]
 
     def __str__(self) -> str:
         """Change the `print()` function message.
 
+        PRIVATE
+        -------
+
         Returns
         -------
         str
-            `print()` the score if it is already compute. Else, `print()` a
-            message to say that the score is not computed.
+            `print()` the distance if it is already compute. Else, `print()` a
+            message to say that the distance is not computed. Do it for all
+            implemented methods.
         """
-        # To check if the score is compute.
-        if self.score:
-            return f"Score is equal to {self.score}."
+        algo_name: "list[str]" = ["O(NP) sequence algorithm",
+                                  "Sørensen-Dice modified coefficient"]
+        to_print: str = ""
 
-        return "Score note compute."
+        # To check if the distance is compute.
+        for i, distance in enumerate(self.distance):
+            if distance:
+                to_print += (f"- Distance compute with {algo_name[i]} is equal "
+                             f"to \"{distance:.1f}\".\n")
+            else:
+                to_print += (f"- Distance compute with {algo_name[i]} was not "
+                             "compute.\n")
 
-    def compare(self) -> None:
-        """Compute a score to check how much two sequence-context are different.
+        return to_print
+
+    def onp_sequence_comparison(self) -> None:
+        """The next implemented algorithm is available in `An O(NP) Sequence
+        Comparison Algorithm` by Sun WU, Udi MANBER, and Gene MYERS in 
+        Information Processing Letters, or in the next link:
+        https://publications.mpi-cbg.de/Wu_1990_6334.pdf
+
+        Based on two given `list | np.array | str`, the algorithm computes a the
+        difference between those two. It is one the fastest algorithm to do
+        this.
         """
         p: int = -1
         # Size different between seq_a and seq_b
@@ -78,30 +99,33 @@ class Alignment:
             p += 1
 
             for i in range(-p, delta, 1):
-                self.array[i + shift] = self.snake(
+                self.array[i + shift] = self.__snake(
                     i,
                     max(self.array[i - 1 + shift] + 1,
                         self.array[i + 1 + shift])
                 )
 
             for i in range(delta + p, delta, -1):
-                self.array[i + shift] = self.snake(
+                self.array[i + shift] = self.__snake(
                     i,
                     max(self.array[i - 1 + shift] + 1,
                         self.array[i + 1 + shift])
                 )
 
-            self.array[delta + shift] = self.snake(
+            self.array[delta + shift] = self.__snake(
                 delta,
                 max(self.array[delta - 1 + shift] + 1,
                     self.array[delta + 1 + shift])
             )
 
         # Sequence difference and double of p.
-        self.score = delta + 2 * p
+        self.distance[0] = delta + 2 * p
 
-    def snake(self, i: int, y: int) -> int:
+    def __snake(self, i: int, y: int) -> int:
         """Snake function to follow diagonal edges.
+
+        PRIVATE
+        -------
 
         Parameters
         ----------
@@ -124,17 +148,59 @@ class Alignment:
 
         return y
 
+    def sorensen_dice(self):
+        """Sørensen-Dice modified coefficient following the next formula:
+
+        ```
+                     2 × |A Δ B|
+        distance = —————————————
+                     |A| + |B|
+        ```
+
+        In other word, we compute the distance between to finished `np.array`.
+        For this, we compute the symmetric difference instead of the
+        intersection. Note that `|A|` signify the length of a given (computed)
+        set.
+
+        Original paper is here :
+        https://www.royalacademy.dk/Publications/High/295_S%C3%B8rensen,%20Thorvald.pdf
+        """
+        # To transform a string in a `np.array`.
+        if isinstance(self.seq_a, str):
+            seq_a: np.array = np.array([*self.seq_a])
+        else:
+            seq_a: np.array = np.array(self.seq_a)
+
+        # To transform a string in a `np.array`.
+        if isinstance(self.seq_b, str):
+            seq_b: np.array = np.array([*self.seq_b])
+        else:
+            seq_b: np.array = np.array(self.seq_b)
+
+        # This is the modified part of Sørensen-Dice coefficient. Here, we
+        # compute a symmetric difference, instead of a intersection. Like so, we
+        # have a distance between two sequence-context and not a similarity.
+        symmetric_diff: int = np.sum(np.isin(seq_b, seq_a, invert=True))
+        # Compute the coefficient.
+        coef: float = 2 * symmetric_diff / (seq_a.shape[0] + seq_b.shape[0])
+
+        # Add the coefficient to the list.
+        self.distance[1] = coef
+
 
 if __name__ == "__main__":
-    AlignmentMatrix: object = Alignment("chien", "niche")
-    AlignmentMatrix.compare()
-    print(AlignmentMatrix)
+    Context: object = ContextAnalyzer("chienn", "niche")
+    Context.onp_sequence_comparison()
+    Context.sorensen_dice()
+    print(Context)
 
-    AlignmentMatrix: object = Alignment("abc", "abd")
-    AlignmentMatrix.compare()
-    print(AlignmentMatrix)
+    Context: object = ContextAnalyzer("abc", "abd")
+    Context.onp_sequence_comparison()
+    Context.sorensen_dice()
+    print(Context)
 
-    AlignmentMatrix: object = Alignment([201, 147, 21, 45, 307],
-                                        [201, 147, 25, 49, 307])
-    AlignmentMatrix.compare()
-    print(AlignmentMatrix)
+    Context: object = ContextAnalyzer([201, 147, 21, 45, 307],
+                                      [201, 147, 25, 49, 307])
+    Context.onp_sequence_comparison()
+    Context.sorensen_dice()
+    print(Context)
